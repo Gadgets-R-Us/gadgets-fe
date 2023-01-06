ee20eaf7fe424851378f56d31952f7c6b7fde9ca
Kendall McGree <mcgreekendall@gmail.com>
2023-01-05 20:28:28 -0600

    add images to Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,18 @@
+import logo from "../../images/gadgets.png";
+import cartLogo from "../../images/cartLogo.png";
+import menuLogo from "../../images/menuLogo.png";
+
 const Navbar = () => {
   return (
     <header className="Navbar">
+      <img className="Menu Logo" src={menuLogo} alt="Menu button" />
+      <img
+        className="App Logo"
+        src={logo}
+        alt="Website's logo of a phone, watch, laptops"
+      />
       <h1>Gadgets R Us</h1>
+      <img className="Cart Logo" src={cartLogo} alt="Cart button" />
     </header>
   );
 };
