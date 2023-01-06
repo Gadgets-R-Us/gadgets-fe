d39274bb489ddc5a85a8887a7a6fcc68fb1f7ceb
Kendall McGree <mcgreekendall@gmail.com>
2023-01-05 20:43:40 -0600

    complete initial styling of Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,12 +7,14 @@ const Navbar = () => {
   return (
     <StyledNavbar>
       <img className="Menu Logo" src={menuLogo} alt="Menu button" />
-      <img
-        className="App Logo"
-        src={logo}
-        alt="Website's logo of a phone, watch, laptops"
-      />
-      <h1>Gadgets R Us</h1>
+      <div className="AppInfo">
+        <img
+          className="App Logo"
+          src={logo}
+          alt="Website's logo of a phone, watch, laptops"
+        />
+        <h1>Gadgets R Us</h1>
+      </div>
       <img className="Cart Logo" src={cartLogo} alt="Cart button" />
     </StyledNavbar>
   );
--- src/components/Navbar/styledNavbar.tsx
+++ src/components/Navbar/styledNavbar.tsx
@@ -3,9 +3,17 @@ import styled from "styled-components";
 export const StyledNavbar = styled.section`
   display: flex;
   flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
 
   .Logo {
     height: 2rem;
     width: 2rem;
+  }
+
+  .AppInfo {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
   }
 `;
