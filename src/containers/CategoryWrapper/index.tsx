961327a8c65d95c944b42149d6a342beb69b51f4
Kendall McGree <mcgreekendall@gmail.com>
2023-01-06 13:48:07 -0600

    create CategoryWrapper container

---
+++ src/containers/CategoryWrapper/index.tsx
@@ -0,0 +1,11 @@
+const CategoryWrapper = () => {
+  return (
+    <>
+      <h1>Category</h1>
+      <select>
+        <option>--Sort By--</option>
+      </select>
+    </>
+  );
+};
+export default CategoryWrapper;
