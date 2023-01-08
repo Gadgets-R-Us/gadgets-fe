203c0410146a03daaeef85c9749eced2a2fef020
Kendall McGree <mcgreekendall@gmail.com>
2023-01-08 17:53:27 -0600

    create Item component for future display

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,21 @@
+const Item = () => {
+  //   console.log(item, "item<<");
+  return (
+    <>
+      <h1>Item</h1>
+      {/* <h2>{item.name}</h2> */}
+      {/* <h2>{name}</h2>
+        <div className="ItemMain">
+            <p>{stars}</p>
+            <h2>{price}</h2>
+        </div>
+        <p>{reviews}</p>
+        <h2>About this Item</h2>
+        <h3>{description}</h3>
+        <h2>Specifications</h2>
+        <ul>{specifications}</ul> */}
+      <button>Add to Cart</button>
+    </>
+  );
+};
+export default Item;
