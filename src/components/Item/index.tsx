7c112b51c19c7855a5a9dca3a3e44382197d0dc8
Kendall McGree <mcgreekendall@gmail.com>
2023-01-08 19:21:21 -0600

    add initial styling to Item component

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,20 +1,29 @@
 import { useLocation } from "react-router-dom";
+import { StyledItem } from "./styledItem";
 const Item = () => {
   const location = useLocation();
+  //destructuring location.state.item below which is coming from Link's state
+  const params = location.state.item;
+
+  const specs = params.specifications.map((spec: string) => {
+    return <li>{spec}</li>;
+  });
+
   return (
-    <>
-      <h2>{location.state.item.name}</h2>
+    <StyledItem>
+      <img className="ItemImage" src={params.image} alt={params.name} />
+      <h2>{params.name}</h2>
       <div className="ItemMain">
-        <p>{location.state.item.starCount}</p>
-        <h2>{location.state.item.price}</h2>
+        <p>{params.starCount}</p>
+        <h2>{params.price}</h2>
       </div>
-      <p>{location.state.item.reviewCount}</p>
+      <p>{params.reviewCount}</p>
       <h2>About this Item</h2>
-      <h3>{location.state.item.longDescription}</h3>
+      <h3>{params.longDescription}</h3>
       <h2>Specifications</h2>
-      <ul>{location.state.item.specifications}</ul>
+      <ul className="SpecList">{specs}</ul>
       <button>Add to Cart</button>
-    </>
+    </StyledItem>
   );
 };
 export default Item;
---
+++ src/components/Item/styledItem.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+export const StyledItem = styled.section`
+  width: 80vw;
+  margin-left: 10vw;
+
+  .ItemImage {
+    height: 15rem;
+    width: 15rem;
+  }
+
+  .ItemMain {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
