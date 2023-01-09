38cd1382dbdbbfac1dda22f018b74d20d310913a
Kendall McGree <mcgreekendall@gmail.com>
2023-01-08 18:56:54 -0600

    pass state as props through router

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,5 +1,6 @@
 import { TItem } from "../../containers/CategoryWrapper";
 import { StyledCategory } from "./styledCategory";
+import { Link } from "react-router-dom";
 
 const Category = ({
   image,
@@ -10,18 +11,22 @@ const Category = ({
   price,
   item,
 }: TItem) => {
-  // console.log(item);
   return (
-    <StyledCategory>
-      <img className="ItemImage" src={image} alt={name} />
-      <h2>{name}</h2>
-      <div className="ItemReputation">
-        <p>{starCount}</p>
-        <p>({reviewCount})</p>
-      </div>
-      <p>Color: {color}</p>
-      <p>${price}</p>
-    </StyledCategory>
+    <Link
+      to={`/item/${name.split(" - ")[1].split(" ").join("_")}`}
+      state={{ item: item }}
+    >
+      <StyledCategory>
+        <img className="ItemImage" src={image} alt={name} />
+        <h2>{name}</h2>
+        <div className="ItemReputation">
+          <p>{starCount}</p>
+          <p>({reviewCount})</p>
+        </div>
+        <p>Color: {color}</p>
+        <p>${price}</p>
+      </StyledCategory>
+    </Link>
   );
 };
 export default Category;
--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,19 +1,18 @@
+import { useLocation } from "react-router-dom";
 const Item = () => {
-  //   console.log(item, "item<<");
+  const location = useLocation();
   return (
     <>
-      <h1>Item</h1>
-      {/* <h2>{item.name}</h2> */}
-      {/* <h2>{name}</h2>
-        <div className="ItemMain">
-            <p>{stars}</p>
-            <h2>{price}</h2>
-        </div>
-        <p>{reviews}</p>
-        <h2>About this Item</h2>
-        <h3>{description}</h3>
-        <h2>Specifications</h2>
-        <ul>{specifications}</ul> */}
+      <h2>{location.state.item.name}</h2>
+      <div className="ItemMain">
+        <p>{location.state.item.starCount}</p>
+        <h2>{location.state.item.price}</h2>
+      </div>
+      <p>{location.state.item.reviewCount}</p>
+      <h2>About this Item</h2>
+      <h3>{location.state.item.longDescription}</h3>
+      <h2>Specifications</h2>
+      <ul>{location.state.item.specifications}</ul>
       <button>Add to Cart</button>
     </>
   );
