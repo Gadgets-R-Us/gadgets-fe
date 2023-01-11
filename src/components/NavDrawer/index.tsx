7f43d89a5623208c3b9c3678eadad03e2511368b
Kendall McGree <mcgreekendall@gmail.com>
2023-01-11 16:45:56 -0600

    create NavDrawer MUI component

---
+++ src/components/NavDrawer/index.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "../../images/menuLogo.png";
+
+type Anchor = "left";
+
+export default function NavDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const handleClick = (event: any) => {
+    console.log(event.target.innerText, "event");
+  };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      //   sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Drones", "Scooters", "Consoles"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton id={text} onClick={handleClick}>
+              <ListItemIcon>
+                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  console.log("working?");
+  return (
+    <div>
+      <React.Fragment key={"left"}>
+        <Button onClick={toggleDrawer("left", true)}>
+          {/* <img src={MenuIcon} /> */}
+          {"image here"}
+        </Button>
+        <Drawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer("left", false)}
+        >
+          {list("left")}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
+}
