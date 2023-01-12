bde9eb26e6a6f9e9ca8ac034e7670792ef30880a
Kendall McGree <mcgreekendall@gmail.com>
2023-01-11 18:39:58 -0600

    add elements and styling to CartWrapper container

--- src/containers/CartWrapper/index.tsx
+++ src/containers/CartWrapper/index.tsx
@@ -3,7 +3,20 @@ import { StyledCartWrapper } from "./styledCartWrapper";
 const CartWrapper = () => {
   return (
     <StyledCartWrapper>
-      <h2>Cart</h2>
+      <h2 className="Header">Cart</h2>
+      <div className="CartSection">
+        <h3>Subtotal</h3>
+        <h3>$</h3>
+      </div>
+      <div className="CartSection">
+        <h3>Shipping</h3>
+        <h3>$</h3>
+      </div>
+      <div className="CartSection">
+        <h3>Total (USD)</h3>
+        <h3>$</h3>
+      </div>
+      <button className="Checkout">Checkout</button>
     </StyledCartWrapper>
   );
 };
